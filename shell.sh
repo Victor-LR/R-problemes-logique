1eb60a0538c79c520f0af7b82e9a6add9cc6b603
Matthias <matthias.launay@hotmail.com>
2021-01-07 17:45:48 +0100

    update shell

--- shell.sh
+++ shell.sh
@@ -2,8 +2,10 @@
 
 make
 ./parser.exe $1
-length=`expr length "$1"`
-subseq=$(echo $1|cut -c-$(($length-2)))
+basename "$1"
+f="$(basename -- $1)"
+length=`expr length $f`
+subseq=$(echo $f|cut -c-$(($length-2)))
 
 g++ -std=c++14 $subseq"cpp" -o $subseq"exe"
 ./$subseq"exe"
